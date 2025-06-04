c80e7a8658087f2c04309caf3b5e864776686a55
hay013 <haymatt013@gmail.com>
2025-06-04 23:26:57 +0700

    Create 1_EDA.py

---
+++ pages/1_EDA.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title("Exploratory Data Analysis (EDA)")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv("healthcare-dataset-stroke-data.csv")
+
+df = load_data()
+st.write("### Dataset")
+st.dataframe(df.head())
+
+st.write("### Informasi Umum")
+st.write(df.describe())
+
+st.write("### Distribusi Stroke")
+fig, ax = plt.subplots()
+sns.countplot(x='stroke', data=df, ax=ax)
+st.pyplot(fig)
+
+st.write("### Korelasi Fitur")
+fig, ax = plt.subplots(figsize=(10,8))
+sns.heatmap(df.corr(), annot=True, fmt=".2f", cmap="coolwarm", ax=ax)
+st.pyplot(fig)
